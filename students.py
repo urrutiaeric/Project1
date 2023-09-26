be921b99d219877058d7e1bf9cb5b52100d9b55f
urrutiaeric <urrutiaeric6@gmail.com>
2023-09-26 13:29:40 -0500

    just updating stuff

---
+++ students.py
@@ -0,0 +1,13 @@
+class students():
+    def getname(name):
+        return name
+    
+    def getidnum(idnum):
+        return idnum
+    
+    def getcourses(courses):
+        return courses
+    
+    def getgpa(gpa):
+        return gpa
+    
---
+++ teacher.py
@@ -0,0 +1,6 @@
+class teacher():
+    def get_teacher_id(teacher_id):
+        return teacher_id
+    
+    def get_teacher_name(teacher_name):
+        return teacher_name
