b895b0bc3d8fc58c9ef156da43e6e238a29fd147
urrutiaeric <urrutiaeric6@gmail.com>
2023-10-03 11:38:06 -0500

    slight changes

--- code/students.py
+++ code/students.py
@@ -1,13 +1,45 @@
+import sqlite3
 class Students:
-    def __init__(self, name, idnum, courses, gpa):
+
+    def __init__(self, name, idnum, courses, gpa, database, table):
         self.name = name
         self.idnum = idnum
         self.courses = courses
         self.gpa = gpa
+        self.database = "student.db"
+        self.table = "Student"
+        
+    def get_connection(self.database):
+        try:
+            conn = sqlite3.connect(self.database)
+            return conn
+        except sqlite3.Error as e:
+            print(e)
+            return None
+
+    
+    def create_table(self.database, self.table):
+        conn = Course.get_connection(self.database)
+        if conn:
+            try:
+                cursor = conn.cursor()
+                cursor.execute(f'''
+                    CREATE TABLE IF NOT EXISTS {self.table} (
+                        id INTEGER PRIMARY KEY,
+                        student_name TEXT,
+                        student id INTEGER,
+                        student_courses TEXT,
+                        student_gpa FLOAT
+                    )
+                ''')
+                conn.commit()
+                conn.close()
+            except sqlite3.Error as e:
+                print(e)
+
+    def get_name(name):
         
 
-    def getname(name):
-        name = input("Student Name: ")
         return name
     
     def getidnum(idnum):
@@ -15,7 +47,7 @@ class Students:
         return idnum
     
     def getcourses(courses):
-        
+
         return courses
     
     def getgpa(gpa):
